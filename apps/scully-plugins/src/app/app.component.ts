150489ba6de1b7efc75952eb4937891f76e96005
Preston Lamb <preston.j.lamb@gmail.com>
2022-01-29 22:34:46 -0700

    initial commit

---
+++ apps/scully-plugins/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'scully-plugins-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'scully-plugins';
+}
